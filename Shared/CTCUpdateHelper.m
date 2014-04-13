cd2650e6bb551383c8091022a774ea931e41f265
Dominik Wagner <dom@codingmonkeys.de>
2014-04-13 16:58:46 +0200

    [FIX] using dispatch_walltime now so no more drifting!

--- Shared/CTCUpdateHelper.m
+++ Shared/CTCUpdateHelper.m
@@ -18,20 +18,38 @@
 }
 
 /*! checks the block if it has granularity then performs it if it has and reschedules it at the next stop at that granularity (e.g. every second, minute or hour) the constants (kCTCUpdateGranularitySeconds, etc.) refer to a factor on seconds - e.g. if you want a granularity of half a second you can specify 0.5 if you want */
-+ (void)performAndScheduleBlock:(dispatch_block_t)aBlock {
-	CTCUpdateHelperGranularity granularity = [self granularityMarkOfBlock:aBlock];
++ (BOOL)performAndScheduleBlock:(dispatch_block_t)aBlock {
+	CTCUpdateHelperGranularity granularity = [self performBlock:aBlock];
 	if (granularity != kCTCUpdateGranularityStop) {
-		aBlock();
-		
 		NSTimeInterval currentTimeInterval = [NSDate timeIntervalSinceReferenceDate];
 		double factor = granularity;
 		double roundedUpTimeInterval = ceil(currentTimeInterval / factor) * factor;
 		NSTimeInterval delayInSeconds = roundedUpTimeInterval - currentTimeInterval;
-		
-		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-			[self performAndScheduleBlock:aBlock];
+		dispatch_queue_t mainQueue = dispatch_get_main_queue();
+		dispatch_after(dispatch_walltime(NULL, (int64_t)(delayInSeconds * NSEC_PER_SEC)), mainQueue, ^{
+			dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, DISPATCH_TIMER_STRICT, mainQueue);
+			dispatch_source_set_event_handler(timer, ^{
+				if ([self performBlock:aBlock] == kCTCUpdateGranularityStop) {
+					dispatch_source_cancel(timer);
+				};
+			});
+			uint64_t interval = ceil(factor * NSEC_PER_SEC); // sadly seems to loose a millisecond every x calls
+															 //			NSLog(@"%s sceduling timer: %@ for interval: %llud (factor: %0.5f)",__FUNCTION__,timer, interval, factor);
+			dispatch_source_set_timer(timer, dispatch_walltime(NULL, 0), interval, 0);
+			dispatch_resume(timer);
 		});
+		return YES;
+	} else {
+		return NO;
 	}
+}
+
++ (CTCUpdateHelperGranularity)performBlock:(dispatch_block_t)aBlock {
+	CTCUpdateHelperGranularity granularity = [self granularityMarkOfBlock:aBlock];
+	if (granularity != kCTCUpdateGranularityStop) {
+		aBlock();
+	}
+	return granularity;
 }
 
 + (void)cancelRegularExecutionOfBlock:(dispatch_block_t)aBlock {
